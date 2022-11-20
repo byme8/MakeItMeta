cf20d3f851000be9fde9c95eb9a5be46a7ca26b9
stanislav <silin.stanislav@outlook.com>
2022-11-20 13:57:30 +0200

    Add refactor tests.

--- src/MakeItMeta.Tests/Cli/InjectionCommandTests.cs
+++ src/MakeItMeta.Tests/Cli/InjectionCommandTests.cs
@@ -131,71 +131,38 @@ public class InjectionCommandTests
     [Fact]
     public async Task CanWorkWithoutInjection()
     {
-        var config =
-            """
-        {
-            "targetAssemblies": []
-        }
-        """;
+        var config = """
+            {
+                "targetAssemblies": []
+            }
+            """;
 
-        var metaAttributesReference = MetadataReference.CreateFromFile("MakeItMeta.Attributes.dll");
-        var projectWithReferenceToMetaReference = await TestProject.Project
-            .AddMetadataReference(metaAttributesReference)
-            .AddDocument("TestAppMetaAttribute.cs",
-                """
+        var attribute = """
                 using MakeItMeta.Attributes;
+                using MakeItMeta.Tests;
                 
                 public class TestAppMetaAttribute : MetaAttribute
                 {
-                    public static void OnEntry(object? @this, string assemblyFullName, string methodName, object?[]? parameters)
+                    public static TestAttribute.Entry? OnEntry(object? @this, string assemblyFullName, string methodName, object?[]? parameters)
                     {
-                       
+                       return TestAttribute.OnEntry(@this, assemblyFullName, methodName, parameters);
                     }
 
-                    public static void OnExit(object? @this, string assemblyFullName, string methodName)
+                    public static void OnExit(object? @this, string assemblyFullName, string methodName, TestAttribute.Entry? entry)
                     {
-
+                       TestAttribute.OnExit(@this, assemblyFullName, methodName, entry);
                     }
                 }            
-                """)
-            .Project
-            .ReplacePartOfDocumentAsync("Program.cs", ("public object? Execute()", "[TestAppMeta]public object? Execute()"));
+                """;
 
-        var testAssembly = await projectWithReferenceToMetaReference.CompileToRealAssemblyAsBytes();
-        var tempTargetAssemblyFile = Path.GetTempFileName();
-        await File.WriteAllBytesAsync(tempTargetAssemblyFile, testAssembly);
+        await Execute(attribute, ("public object? Execute()", "[TestAppMeta]public object? Execute()"), config);
 
-        config = config.Replace(@"""targetAssemblies"": []", @$"""targetAssemblies"": [""{tempTargetAssemblyFile}""]");
-        var tempConfigFile = Path.GetTempFileName();
-        await File.WriteAllTextAsync(tempConfigFile, config);
-
-        var command = new InjectCommand();
-        var console = new FakeInMemoryConsole();
-        command.Config = tempConfigFile;
-
-        await command.ExecuteAsync(console);
-
-        var modifiesAssemblyBytes = await File.ReadAllBytesAsync(tempTargetAssemblyFile);
-        var modifiedAssembly = Assembly.Load(modifiesAssemblyBytes);
-
-        _ = modifiedAssembly.Execute();
-
-        var outputString = console.ReadOutputString();
-        var errorString = console.ReadErrorString();
-        await Verify(new
-            {
-                outputString,
-                errorString
-            })
-            .Track(tempTargetAssemblyFile)
-            .Track(tempConfigFile);
     }
-    
+
     [Fact]
     public async Task MetaAttributesAreIgnored()
     {
-        var config =
-            """
+        var config = """
             {
                 "targetAssemblies": [],
                 "attributes": 
@@ -208,30 +175,45 @@ public class InjectionCommandTests
             }
             """;
 
-        var metaAttributesReference = MetadataReference.CreateFromFile("MakeItMeta.Attributes.dll");
-        var projectWithReferenceToMetaReference = await TestProject.Project
-            .AddMetadataReference(metaAttributesReference)
-            .AddDocument("TestAppMetaAttribute.cs",
-                """
+        var attribute = """
                 using MakeItMeta.Attributes;
-                
+                using MakeItMeta.Tests;
+
                 public class TestAppMetaAttribute : MetaAttribute
                 {
-                    public static void OnEntry(object? @this, string assemblyFullName, string methodName, object?[]? parameters)
+                    public static TestAttribute.Entry? OnEntry(object? @this, string assemblyFullName, string methodName, object?[]? parameters)
                     {
-                       
+                       return TestAttribute.OnEntry(@this, assemblyFullName, methodName, parameters);
                     }
 
-                    public static void OnExit(object? @this, string assemblyFullName, string methodName)
+                    public static void OnExit(object? @this, string assemblyFullName, string methodName, TestAttribute.Entry? entry)
                     {
-
+                       TestAttribute.OnExit(@this, assemblyFullName, methodName, entry);
                     }
                 }            
-                """)
-            .Project
-            .ReplacePartOfDocumentAsync("Program.cs", ("public object? Execute()", "[TestAppMeta]public object? Execute()"));
+                """;
 
-        var testAssembly = await projectWithReferenceToMetaReference.CompileToRealAssemblyAsBytes();
+        await Execute(attribute, default, config);
+    }
+
+    private static async Task Execute(string newFile, (string, string) places, string config)
+    {
+        var metaAttributesReference = MetadataReference.CreateFromFile("MakeItMeta.Attributes.dll");
+        var metaATestsReference = MetadataReference.CreateFromFile("MakeItMeta.Tests.dll");
+        var console = new FakeInMemoryConsole();
+
+        var project = TestProject.Project
+            .AddMetadataReference(metaAttributesReference)
+            .AddMetadataReference(metaATestsReference)
+            .AddDocument("Test.cs", newFile)
+            .Project;
+
+        if (places != default)
+        {
+            project = await project.ReplacePartOfDocumentAsync("Program.cs", places);
+        }
+
+        var testAssembly = await project.CompileToRealAssemblyAsBytes();
         var tempTargetAssemblyFile = Path.GetTempFileName();
         await File.WriteAllBytesAsync(tempTargetAssemblyFile, testAssembly);
 
@@ -240,7 +222,6 @@ public class InjectionCommandTests
         await File.WriteAllTextAsync(tempConfigFile, config);
 
         var command = new InjectCommand();
-        var console = new FakeInMemoryConsole();
         command.Config = tempConfigFile;
 
         await command.ExecuteAsync(console);
@@ -249,11 +230,15 @@ public class InjectionCommandTests
         var modifiedAssembly = Assembly.Load(modifiesAssemblyBytes);
 
         _ = modifiedAssembly.Execute();
-
+        
+        var modifiedAssemblyFullName = modifiedAssembly.FullName!;
+        var calls = TestAttribute.MethodsByAssembly.GetValueOrDefault(modifiedAssemblyFullName);
         var outputString = console.ReadOutputString();
         var errorString = console.ReadErrorString();
+        
         await Verify(new
             {
+                calls,
                 outputString,
                 errorString
             })
